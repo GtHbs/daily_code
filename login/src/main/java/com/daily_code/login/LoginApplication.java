c4d73f180e3176559c55cdff55c925798e8288f8
Kerwinnli <Kerwinnli@tencent.com>
2024-01-31 14:09:13 +0800

    init

---
+++ login/src/main/java/com/daily_code/login/LoginApplication.java
@@ -0,0 +1,15 @@
+package com.daily_code.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Kerwinnli
+ * @date: 2024/1/31 11:48
+ */
+@SpringBootApplication
+public class LoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+}
